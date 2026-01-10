a62e2beb04175172569c08bca8a9b677963d66f1
shahd-moussa <moussashahd2@gmail.com>
2026-01-10 18:35:20 +0300

    customer app created

---
+++ restaurant/customers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
