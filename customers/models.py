bc647b699e3382159ae6a6c04e283f8e00d01242
shahd-moussa <moussashahd2@gmail.com>
2026-01-24 14:08:21 +0300

    Merge cutomers/models, serializers and tests from shahd/customer to develop

--- customers/models.py
+++ customers/models.py
@@ -1,12 +1,37 @@
 from django.db import models
+from django.core.validators import RegexValidator, EmailValidator
+from django.core.exceptions import ValidationError
+
+
+phone_validator = RegexValidator(
+    regex=r'^\d{10}$',
+    message="Phone number must be exactly 10 digits."
+)
+
+email_validator = EmailValidator( message="Enter a valid email address.")
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20)
+    email = models.EmailField(
+        unique=True,
+        validators=[email_validator]
+    )
+    phone = models.CharField(
+        max_length=10,
+        validators=[phone_validator]
+    )
     address = models.TextField()
     registration_date = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+
+        if len(self.first_name) < 2:
+            raise ValidationError("First name must be at least 2 characters long.")
+
+        if len(self.last_name) < 2:
+            raise ValidationError("Last name must be at least 2 characters long.")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- customers/tests.py
+++ customers/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from customers.models import Customer
+
+User = get_user_model()
+
+
+class CustomerAPITest(APITestCase):
+
+    def setUp(self):
+        # Users
+        self.admin = User.objects.create_user(
+            username='admin',
+            password='admin12345',
+            role='admin'
+        )
+
+        self.manager = User.objects.create_user(
+            username='manager',
+            password='manager12345',
+            role='manager'
+        )
+
+        self.staff = User.objects.create_user(
+            username='staff',
+            password='staff12345',
+            role='staff'
+        )
+
+        self.customer_data = {
+            "first_name": "Sara",
+            "last_name": "Ali",
+            "email": "sara@test.com",
+            "phone": "0999999999",
+            "address": "Damascus"
+        }
+
+        self.url = "/api/customers/customers/"
+
+    def test_admin_can_create_customer(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.post(self.url, self.customer_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Customer.objects.count(), 1)
+
+
+    def test_manager_can_view_customers(self):
+        Customer.objects.create(**self.customer_data)
+        self.client.force_authenticate(user=self.manager)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_staff_cannot_access_customers(self):
+        self.client.force_authenticate(user=self.staff)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    def test_invalid_phone_validation(self):
+        self.client.force_authenticate(user=self.admin)
+
+        invalid_data = self.customer_data.copy()
+        invalid_data["phone"] = "123"
+
+        response = self.client.post(self.url, invalid_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_first_name_min_length_validation(self):
+        self.client.force_authenticate(user=self.admin)
+        invalid_data = self.customer_data.copy()
+        invalid_data["first_name"] = "A"
+        response = self.client.post(self.url, invalid_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
